ca4d6c6d87d27f5b1aeb2e29fac0799e38362fe1
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-07-12 17:52:33 +0000

    Tasks can now be shown as completed

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -2,7 +2,8 @@ import React, {useState, useEffect} from 'react'
 
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState('');
-  const [listItems, setListItems] = useState([{task: "A thing that needs done", priority: false}]);
+  const [listItems, setListItems] = useState([{task: "This task is a priority", priority: true}, {task: "This task is not a priority", priority: false}]);
+  const [completed, setCompleted] = useState(['This task is complete']);
 
   useEffect(() => {
     console.log(listItems);
@@ -21,29 +22,55 @@ const ToDoList = () => {
   const prioritized = (obj, idx) => {
     obj.priority = !obj.priority;
     setListItems(listItems.splice(idx,1));
-    if (obj.priority === true)setListItems([obj,...listItems])
-    else setListItems([...listItems, obj])
+    obj.priority ? setListItems([obj,...listItems]) : setListItems([...listItems, obj])
+  }
+
+  const taskComplete = (obj, idx)  => {
+    setListItems(listItems.toSpliced(idx,1));
+    setCompleted([...completed, obj.task]);
+    console.log(completed);
   }
 
   return (
-    <div className='accordion w-50 m-auto'>
+    <div className='accordion w-75 m-auto'>
         <div className='accordion-item p-1'>
             <input className='w-75' type='text' onChange={e => setInputValue(e.target.value)} onKeyDown={addItem} value={inputValue} />
         </div>
         {listItems.map((item, idx) => {
             return (
                 <div key={idx} className={`accordion-item px-2 py-1 d-flex justify-content-between align-items-center ${item.priority ? "bg-warning-subtle" : ""}`}>
-                    <strong>{item.priority ? "* " : ""}{item.task}</strong>
+                    <strong>{item.priority ? "! " : ""}{item.task}</strong>
                     <div>
+                      <button className="btn btn-success me-1" onClick={() => taskComplete(item, idx)} title='Task Is Complete'>
+                        <i className="fa-solid fa-check"></i>
+                      </button>
                       <button className="btn btn-warning me-1" onClick={() => prioritized(item, idx)} title={item.priority ? "Make Not A Priority" : "Make Priority"}>
                         <i className="fa-solid fa-circle-exclamation" style={{color: "#fff"}}></i>
                       </button>
-                      <button className='btn btn-danger' onClick={() => setListItems(listItems.toSpliced(idx, 1))} title='Delete'>
+                      <button className='btn btn-danger' onClick={() => setListItems(listItems.toSpliced(idx, 1))} title='Delete' disabled={item.priority ? true : false}>
                           <i className="fa-solid fa-trash-can"></i>
                       </button>
                     </div>
                 </div>
             )
+        })}
+        {completed.map((item, idx) => {
+          return (
+            <div key={idx} className='accordion-item px-2 py-1 d-flex justify-content-between align-items-center bg-secondary-subtle'>
+              <strong><s>{item}</s></strong>
+              <div>
+                <button className="btn btn-secondary me-1" disabled>
+                  <i className="fa-solid fa-check" style={{color: "#bbb"}}></i>
+                </button>
+                <button className="btn btn-secondary me-1" disabled>
+                  <i className="fa-solid fa-circle-exclamation" style={{color: "#bbb"}}></i>
+                </button>
+                <button className="btn btn-danger" onClick={() => setCompleted(completed.toSpliced(idx, 1))}>
+                  <i className="fa-solid fa-trash-can"></i>
+                </button>
+              </div>
+            </div>
+          )
         })}
     </div>
   )
