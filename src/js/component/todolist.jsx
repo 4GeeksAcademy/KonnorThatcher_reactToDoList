05545f4fb947ee08f6b2b3d2536f4a9d3e031074
KonnorThatcher <131403452+KonnorThatcher@users.noreply.github.com>
2023-07-12 02:39:13 +0000

    List items can now be prioritized

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 const ToDoList = () => {
   const [inputValue, setInputValue] = useState('');
-  const [listItems, setListItems] = useState(["A thing that needs done"]);
+  const [listItems, setListItems] = useState([{task: "A thing that needs done", priority: false}]);
 
   useEffect(() => {
     console.log(listItems);
@@ -10,9 +10,19 @@ const ToDoList = () => {
 
   const addItem = (event) => {
     if (event.key === 'Enter') {
-        if (inputValue.length > 0) setListItems([...listItems,inputValue]);
+        if (inputValue.length > 0){
+          let newObj = {task: inputValue, priority: false}
+          setListItems([...listItems,newObj])
+        };
         setInputValue('');
     }
+  }
+
+  const prioritized = (obj, idx) => {
+    obj.priority = !obj.priority;
+    setListItems(listItems.splice(idx,1));
+    if (obj.priority === true)setListItems([obj,...listItems])
+    else setListItems([...listItems, obj])
   }
 
   return (
@@ -22,11 +32,16 @@ const ToDoList = () => {
         </div>
         {listItems.map((item, idx) => {
             return (
-                <div key={idx} className='accordion-item px-2 py-1 d-flex justify-content-between align-items-center'>
-                    <strong>{item}</strong>
-                    <button className='btn btn-danger' onClick={() => setListItems(listItems.toSpliced(idx, 1))}>
-                        <i className="fa-solid fa-trash-can" title='Delete'></i>
-                    </button>
+                <div key={idx} className={`accordion-item px-2 py-1 d-flex justify-content-between align-items-center ${item.priority ? "bg-warning-subtle" : ""}`}>
+                    <strong>{item.priority ? "* " : ""}{item.task}</strong>
+                    <div>
+                      <button className="btn btn-warning me-1" onClick={() => prioritized(item, idx)} title={item.priority ? "Make Not A Priority" : "Make Priority"}>
+                        <i className="fa-solid fa-circle-exclamation" style={{color: "#fff"}}></i>
+                      </button>
+                      <button className='btn btn-danger' onClick={() => setListItems(listItems.toSpliced(idx, 1))} title='Delete'>
+                          <i className="fa-solid fa-trash-can"></i>
+                      </button>
+                    </div>
                 </div>
             )
         })}
